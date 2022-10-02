60963c6b261b1a1ba420d5715131ff07722552c0
Zhanxun Liu <xcc_zach@sjtu.edu.cn>
2022-10-02 15:27:39 +0800

    appbar added

--- xcc-homepage/src/App.tsx
+++ xcc-homepage/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Grid, AppBar, Toolbar, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = ()=>{
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Grid container>
+      <Grid item xs={12}>
+        <AppBar>
+          <Toolbar>
+            <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+              <MenuIcon></MenuIcon>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App
