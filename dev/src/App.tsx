3ae5f81046e8d68cca0019869c78148b76320b0c
Zhanxun Liu <xcc_zach@sjtu.edu.cn>
2022-10-02 22:22:14 +0800

    doc system complete

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -1,6 +1,8 @@
-import { Grid, AppBar, Toolbar, IconButton, Menu, MenuItem, Autocomplete, TextField } from '@mui/material';
+import { Grid, AppBar, Toolbar, IconButton, Menu, MenuItem, Autocomplete, TextField, List, ListItem, ListItemButton, ListItemText, Typography, Divider } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { useState } from 'react';
+import { ArrowBackIos } from '@mui/icons-material';
+import React, { useEffect, useState } from 'react';
+import { Remarkable } from 'remarkable'
 
 enum PageState { MainPage, DocPage };
 
@@ -33,8 +35,9 @@ const App = () => {
             </IconButton>
           </Toolbar>
         </AppBar>
-        <Toolbar/>
+        <Toolbar />
         <Menu anchorEl={anchorEl} open={hasAnchor} onClose={closeMainMenu}>
+          <MenuItem onClick={changePage(PageState.MainPage)}>主页</MenuItem>
           <MenuItem onClick={changePage(PageState.DocPage)}>文档检索</MenuItem>
         </Menu>
       </Grid>
@@ -70,27 +73,101 @@ const MainPage = () => {
   );
 }
 
-const getDocNameList = ()=>{
-  const req = new XMLHttpRequest();
-  let text: string = "";
-  req.onload = function(){
-    text = this.responseText;
+const DocPage = () => {
+  const [docList, setDocList] = useState<string[]>([]);
+  useEffect(() => {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if (req.readyState == 4 && req.status == 200) {
+        setDocList(JSON.parse(req.responseText).doclist)
+      }
+    }
+    req.open("GET", "/documents/doclist.json?t=" + Math.random(), true);
+    req.send();
+  }, []);
+  const [isNavigating, setIsNavigating] = useState(true);
+  const [targetDoc, setTargetDoc] = useState("");
+  const openDoc = (target: string) => {
+    setIsNavigating(false);
+    setTargetDoc(target);
   }
-  req.open("GET","documents/sample.txt",true);
-  req.send();
-  return text;
+  const closeDoc = () => {
+    setIsNavigating(true);
+    setTargetDoc("");
+  }
+
+  if (isNavigating) {
+    return (<DocNaviPage docList={docList} onClick={openDoc}></DocNaviPage>);
+  } else {
+    return (<DocShowPage docName={targetDoc} onBack={closeDoc}></DocShowPage>);
+  }
 }
 
-const DocPage = () => {
+const DocNaviPage = ({ docList, onClick }: { docList: string[], onClick: (targetDoc: string) => void }) => {
   return (
     <Grid container direction="column"
-    justifyContent="flex-start"
-    alignItems="center">
+      justifyContent="flex-start"
+      alignItems="center">
       <Grid item xs={12}>
-        <Autocomplete options={["1","2"]} renderInput={(params)=><TextField {...params}/>} 
-        onChange={(event, newValue)=>{
-          console.log(getDocNameList());
-        }}/>
+        <Autocomplete options={docList.map(item => item.slice(0, item.length - 3))} renderInput={(params) => <TextField {...params} />}
+          onChange={(_event, newValue) => {
+            if (newValue !== null) {
+              setTimeout(() => {
+                onClick(newValue + ".md");
+              }, 100);
+            }
+          }} />
+      </Grid>
+      <Grid item xs={12}>
+        <List>
+          {
+            docList.map(item =>
+              <ListItem key={item}>
+                <ListItemButton onClick={() => {
+                  setTimeout(() => {
+                    onClick(item);
+                  }, 300);
+                }}>
+                  <ListItemText primary={item.slice(0, item.length - 3)}></ListItemText>
+                </ListItemButton>
+              </ListItem>)
+          }
+        </List>
+      </Grid>
+    </Grid>
+  );
+}
+
+const DocShowPage = ({ docName, onBack }: { docName: string, onBack: () => void }) => {
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if (req.readyState == 4 && req.status == 200) {
+        setContent(req.responseText);
+      }
+    }
+    req.open("GET", "/documents/" + docName + "?t=" + Math.random(), true);
+    req.send();
+  }, []);
+  return (
+    <Grid container>
+      <Grid item xs={2}>
+        <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2, mt: 1 }}
+          onClick={() => {
+            setTimeout(() => {
+              onBack();
+            }, 300);
+          }}>
+          <ArrowBackIos></ArrowBackIos>
+        </IconButton>
+      </Grid>
+      <Grid item xs={10}>
+        <Typography variant='h3'>{docName.slice(0, docName.length - 3)}</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Divider></Divider>
+        <div dangerouslySetInnerHTML={{ __html: new Remarkable().render(content) }}></div>
       </Grid>
     </Grid>
   );
