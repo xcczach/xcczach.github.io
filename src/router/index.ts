8a4d1b31b9adcce2fcbb53d768005e35545471f1
xcc <2867389537@qq.com>
2023-04-06 21:17:14 +0800

    add 404 not found

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from "../views/HomeView.vue"
+import HomeView from "@/views/HomeView.vue"
+import NotFoundView from "@/views/NotFoundView.vue"
 import DiscreteMathsView from '@/views/maths/DiscreteMathsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {path: "/:pathMatch(.*)*", component: NotFoundView},
     {path: "/", component: HomeView},
-    {path: "/discrete-maths", component: DiscreteMathsView}
+    {path: "/discrete-maths", component: DiscreteMathsView},
   ]
 })
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        找不到对应界面quq
+    </div>
+</template>
