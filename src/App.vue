9090c8b5128ea37e9678b2bbde7e0b81a29d71f0
xcc <2867389537@qq.com>
2023-04-06 19:29:25 +0800

    add base nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,50 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ElContainer, ElMain, ElAside, ElMenu, ElMenuItem, ElSubMenu, ElMenuItemGroup } from 'element-plus';
 </script>
 
 <template>
+  <ElContainer>
+    <ElAside width="200px" style="background-color: antiquewhite;">
+      <ElMenu>
+        <ElMenuItem>
+          <RouterLink to="/home">
+            欢迎ヾ(•ω•`)o
+          </RouterLink>
+        </ElMenuItem>
+        <ElSubMenu index="1">
+          <template #title>
+            <span>笔记</span>
+          </template>
+          <ElMenuItemGroup title="数学">
+            <ElMenuItem>
+              离散数学
+            </ElMenuItem>
+          </ElMenuItemGroup>
+        </ElSubMenu>
+      </ElMenu>
+    </ElAside>
+    <ElMain style="background-color: aliceblue;">
+      <RouterView />
+    </ElMain>
+  </ElContainer>
 </template>
 
 <style scoped>
+.el-container {
+  height: 100%;
+}
+</style>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from "../views/HomeView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {path: "/home", component: HomeView}
   ]
 })
 
